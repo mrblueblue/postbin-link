8ed0fb7b02c1a3a90eb693b900330eb0ae233432
Jonathan Huang <grokut@gmail.com>
2015-11-22 00:16:28 -0500

    update/ Home component routes to bin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
+import api from '../utils/api';
+import withContext from 'recompose/withContext';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className='home-container'>
-        <div className='home-main'>
-          <h1>PostBin</h1>
-          <h4>Save and Inspect POST Requests</h4>
-          <Link to='bin'>Click</Link>
-          <button>Create Bin</button>
-        </div>
-      </div>
-    );
+const Home = ({history}) => {
+
+  function createBin(){
+    api.getBinId()
+      .then((data) => history.push(`/${data.id}`))
   }
+
+  return (
+    <div className='home-container'>
+      <div className='home-main'>
+        <h1>PostBin</h1>
+        <h4>Save and Inspect POST Requests</h4>
+        <a onClick={createBin}>Click</a>
+        <button>Create Bin</button>
+      </div>
+    </div>
+  );
 }
+
+export default Home
